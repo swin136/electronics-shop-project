d1f8b4b55986d6f720c0bf4fe79badc3edb11e83
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-19 19:15:45 +0300

    add class Phone in src/phone.py

---
+++ src/phone.py
@@ -0,0 +1,32 @@
+from src.item import Item
+
+
+class Phone(Item):
+    """
+    Класс для представления телефона в магазине.
+    """
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+        """
+           Создание экземпляра класса Phone.
+
+           :param name: Название товара.
+           :param price: Цена за единицу товара.
+           :param quantity: Количество товара в магазине.
+           :param number_of_sim: Количество сим-карт в телефоне.
+           """
+        super().__init__(name, price, quantity)
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value: int):
+        if value <= 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self.__number_of_sim = value
+
+    def __repr__(self):
+        """Магический метод для официального "текстового" образа объекта класса"""
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
