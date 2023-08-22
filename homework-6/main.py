47a6942e85ae02fc20368935640828579eae2cb2
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-22 21:35:43 +0300

    add refences to currupt csv-files

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,10 +1,19 @@
 from src.item import Item
+from src.item import InstantiateCSVError
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    Item.csv_file = '..\\src\\no_items.csv'
+    try:
+        Item.instantiate_from_csv()
+    except FileNotFoundError as file_not_exists_err:
+        print(file_not_exists_err)
     # FileNotFoundError: Отсутствует файл item.csv
 
-    # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+    # В файле items_3.csv удалена последняя колонка.
+    Item.csv_file = '..\\src\\items_3.csv'
+    try:
+        Item.instantiate_from_csv()
+    except InstantiateCSVError as inst_csv_error:
+        print(inst_csv_error)
     # InstantiateCSVError: Файл item.csv поврежден
