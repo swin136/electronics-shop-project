cc65691ebe6bf614634d801e3e7b1823a827b18e
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-22 19:02:31 +0300

    add testcase for class Keyboard

---
+++ tests/test_keyboard.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.keyboard import Keyboard
+
+
+def test_keyboard_instance():
+    """Проверяем правильность функционирования экземпляра класса Keyboard"""
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    # Проверяем корректность гетера, наследованного от класса Item
+    assert str(kb) == 'Dark Project KD87A'
+    # Проверяем корректность гетера, наследованного от класса-миксина Keyboard
+    assert str(kb.language) == "EN"
+
+    # Вызываем метод смены раскладки клавиатуры
+    kb.change_lang()
+    assert str(kb.language) == "RU"
+
+    # Сделали последовательно смену раскладок RU -> EN -> RU
+    kb.change_lang().change_lang()
+    assert str(kb.language) == "RU"
+    # Проверяем, что смена раскладки клавиатуры через свойство невозможно
+    with pytest.raises(AttributeError):
+        kb.language = 'CH'
