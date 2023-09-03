ac7ba9380455fed66facf6c88f28330fb02c1851
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-09-03 23:04:21 +0300

    fix pep8 bugs with flake8

--- src/phone.py
+++ src/phone.py
@@ -29,4 +29,5 @@ class Phone(Item):
 
     def __repr__(self):
         """Магический метод для официального "текстового" образа объекта класса"""
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return (f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, "
+                f"{self.number_of_sim})")
